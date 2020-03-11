bc54961a3ed482c45524e0991bb07a846a9fea28
duncanstevenson <dmiester1965@gmail.com>
2020-03-11 12:47:26 -0400

    add some angle calcualtions
    Add Functions:
    get_angle(x1, y1, x2, y2)
    get_absolute_angle_from_orig(x, y)

--- LeahurstCS.py
+++ LeahurstCS.py
@@ -17,9 +17,7 @@ make note of added dependencies
 make note of added functions
 """
 
-def throwaway(x):
-    print("Hello World")
-    
+
 def read_file_contents(filename):
     """
     This function loads all the information in a given file. (remember to include .txt)
@@ -37,7 +35,48 @@ def calc_linear_distance(x1, y1, x2, y2):
     :param x1: x coordinate at point 1
     :param y1: y coordinate at point 1
     :param x2: x coordinate at point 2
-    :param y2:y coordinate at point 2
-    :return: Linear Distance from point to point
+    :param y2: y coordinate at point 2
+    :return: (float) Linear Distance from point to point
     """
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
+def get_angle(x1, y1, x2, y2):
+    """
+    Calculates the angle of the line between the two given coordinates and the x-axis.
+    Will always be between 90 to -90 degrees.
+    :param x1: x coordinate at point 1
+    :param y1: y coordinate at point 1
+    :param x2: x coordinate at point 2
+    :param y2: y coordinate at point 2
+    :return: (float) the Related Acute Angle in degrees
+    """
+    deltaX = x2 - x1
+    deltaY = y2 - y1
+    return math.degrees(math.atan(deltaY / deltaX))
+
+
+def get_absolute_angle_from_orig(x, y):
+    """
+    Calculates the angle of the line from the origin to the given point,
+    always with respect to the positive x axis, from 0-360 degrees
+    :param x: x coordinate at the line end
+    :param y: y coordinate at the line end
+    :return: (float) absolute angle from +x axis in degrees
+    """
+
+    if x > 0 and y > 0:
+        print("Q1")
+        angle = get_angle(0, 0, x, y)
+    elif x > 0 and y < 0:
+        print("Q4")
+        angle = get_angle(0, 0, Px, Py)
+    elif x < 0 and y < 0:
+        print("Q3")
+        angle = get_angle(0, 0, Px, Py) + 180
+    elif x < 0 and y > 0:
+        print("Q2")
+        angle = 180 + get_angle(0, 0, Px, Py)
+    else:
+        angle = 0
+    return angle
